3b04d0008a2517769960aa8b03e4361302b76b58
akanshaj65 <akanshajha573@gmail.com>
2026-06-15 19:10:27 +0530

    Week 3Day 1 Leetcode Solutions

---
+++ Week3/Day1/ReverseLL.java
@@ -0,0 +1,17 @@
+public class ReverseLL {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr=head;
+        ListNode prev=null;
+        ListNode next; 
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+
+        }
+       
+       return prev; 
+    }
+    
+}
